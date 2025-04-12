b50c4d61b446cdf10bc266444cb2f13272787850
Xu Dylan <dtx2@zoo.cs.yale.edu>
2025-04-11 21:50:37 -0400

    Moved focus from game to player

--- game.py
+++ game.py
@@ -4,12 +4,10 @@ from enum_eras import Era
 from player import Player
 class Game():
     def __init__(self):
-        self._current_player: Player = Player(self, "white")
-        self._other_player: Player = Player(self, "black")
-        self._turn = 1
         self._board = Board(4)
-        self._white_focus = Era.PAST
-        self._black_focus = Era.FUTURE
+        self._current_player: Player = Player("white", self._board)
+        self._other_player: Player = Player("black", self._board)
+        self._turn = 1
 
     def update(self, piece: Piece, move_direction1, move_direction2, new_focus_era):
         self._board.update(piece, move_direction1)
@@ -27,19 +25,14 @@ class Game():
             self._black_focus = new_focus_era
         elif self._current_player.get_color() == "white":
             self._white_focus = new_focus_era
-
-    @staticmethod
-    def _indent_focus(focus):
-        if focus == Era.PAST:
-            return ' ' * 2
-        elif focus == Era.PRESENT:
-            return ' ' * 14
-        elif focus == Era.FUTURE:
-            return ' ' * 26
         
     def __str__(self):
-        black_focus = Game._indent_focus(self._black_focus) + "black\n"
-        white_focus = Game._indent_focus(self._white_focus) + "white\n"
+        if self._current_player.get_color() == "black":
+            black_focus = str(self._current_player)
+            white_focus = str(self._other_player)
+        elif self._current_player.get_color() == "white":
+            black_focus = str(self._other_player)
+            white_focus = str(self._current_player)
         turn_player = f"Turn: {self._turn}, Current player: {self._current_player.get_color()}\n"
         result = black_focus + str(self._board) + white_focus + turn_player
         return result
--- player.py
+++ player.py
@@ -1,10 +1,14 @@
-## from game import Game
+from board import Board
 from move_command import MoveCommand
+from enum_eras import Era
 class Player():
-    def __init__(self, game, color: str):
-        self._game = game
+    def __init__(self, color: str, board: Board):
         self._color = color
-        self._pieces = {}
+        if color == "black":
+            self._focus = Era.FUTURE
+        elif color == "white":
+            self._focus = Era.PAST
+        self._board = board
 
     def get_color(self):
         return self._color
@@ -15,7 +19,17 @@ class Player():
     def _enumerate_possible_moves(self):
         pass
     
-    
+    @staticmethod
+    def _indent_focus(focus):
+        if focus == Era.PAST:
+            return ' ' * 2
+        elif focus == Era.PRESENT:
+            return ' ' * 14
+        elif focus == Era.FUTURE:
+            return ' ' * 26
+        
+    def __str__(self):
+        return Player._indent_focus(self._focus) + self._color + "\n"
    
 
 class Human(Player):
